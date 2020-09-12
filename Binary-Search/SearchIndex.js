100374edb5e37ed23c3bef358a7b1530d989e097
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-12 17:00:41 -0400

    Search Index complete

---
+++ Binary-Search/SearchIndex.js
@@ -0,0 +1,58 @@
+// 35. Search Insert Position
+// https://leetcode.com/problems/search-insert-position/
+
+// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You may assume no duplicates in the array.
+
+// Example 1:
+
+// Input: [1,3,5,6], 5
+// Output: 2
+// Example 2:
+
+// Input: [1,3,5,6], 2
+// Output: 1
+// Example 3:
+
+// Input: [1,3,5,6], 7
+// Output: 4
+// Example 4:
+
+// Input: [1,3,5,6], 0
+// Output: 0
+
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+*/
+
+var searchInsert = function(nums, target) {
+    let low = 0
+    let high = nums.length - 1
+
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2)
+        let pointer = nums[mid]
+        let backP = nums[mid - 1]
+        let frontP = nums[mid + 1]
+
+        if (pointer === target) {
+            return mid
+        } else if (pointer < target && frontP > target || pointer < target && mid === nums.length - 1) {
+            return mid + 1
+        } else if (pointer < target) {
+            low = mid + 1
+        } else if (pointer > target && backP < target || pointer > target && mid === 0) {
+            return mid 
+        } else {
+            high = mid - 1
+        }
+    }
+};
+
+input1 = [1, 3, 5, 6];
+input2 = 2
+
+console.log(searchInsert(input1, input2))
