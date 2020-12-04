804b7e2f7cc1118c824a8777c888b44cf477b6a0
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-12-04 10:15:35 -0500

    Memoized Grid Traveler

--- Algorithims/GridTraveler.js
+++ Algorithims/GridTraveler.js
@@ -1,7 +1,11 @@
-const gridTraveler = (m, n) => {
+const gridTraveler = (m, n, memo = {}) => {
+  const key = m + "," + n;
+
+  if (key in memo) return memo[key];
   if (m === 1 && n === 1) return 1;
   if (m === 0 || n === 0) return 0;
-  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+  return memo[key];
 };
 
 console.log(gridTraveler(1, 1));
