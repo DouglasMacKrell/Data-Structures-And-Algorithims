25cb8e0340f875b3b66df1c59f53266d8d94687b
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-12-09 19:54:55 -0500

    swap for Bubble Sort

--- Algorithims/BubbleSort.js
+++ Algorithims/BubbleSort.js
@@ -1,7 +1,24 @@
 // loop backwards over an array and log each element
 
+const input1 = [6, 5, 3, 1, 8, 7, 2, 4]; 
+
 const reverseLoop = (arr) => {
-    for (let i = arr.length; i > 0; i--) {
+    for (let i = arr.length - 1; i > -1; i--) {
         console.log(arr[i])
     }
 }
+
+reverseLoop(input1)
+
+// Write a function `swap` that takes in an array and two indexes `i` &`j` and swaps the elements `I` & `j`.
+
+const swap = (arr, a, b) => {
+    let eleA = arr[a]
+    let eleB = arr[b]
+    arr.splice(a, 2, eleB, eleA)
+    return arr
+}
+
+console.log(swap(input1, 1, 2))
+console.log(swap(input1, 2, 5));
+console.log(swap(input1, 2, 4));
