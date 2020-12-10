20dd9337360875f03331883d3e22e60cda7fcb80
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-12-09 20:59:23 -0500

    bubbleSort3 added

--- Algorithims/BubbleSort.js
+++ Algorithims/BubbleSort.js
@@ -26,25 +26,39 @@ console.log(swap(input1, 2, 4));
 const bubbleSort = (arr) => {
   for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
-        if (arr[j] > arr[j + 1]) {
-            swap(arr, j, j + 1)
-        }
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
     }
   }
 };
 
-
 const bubbleSort2 = (arr) => {
-    let unsorted = true
-    let lastIndex = arr.length - 1
-    while (unsorted) {
-        unsorted = false
-        for (let i = 0; i < lastIndex; i++)
-        if (arr[i] > arr[i + 1]) {
-            swap(arr, i, i + 1)
-            unsorted = true
-        }
-        lastIndex -= 1
-        
+  let unsorted = true;
+  let lastIndex = arr.length - 1;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < lastIndex; i++)
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        unsorted = true;
+      }
+    lastIndex -= 1;
+  }
+};
+
+const bubbleSort3 = (arr) => {
+  let lastIndex = arr.length - 1;
+  let swapped = true;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < lastIndex; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        swapped = true;
+      }
     }
-}
+    lastIndex--;
+  } while (swapped);
+};
